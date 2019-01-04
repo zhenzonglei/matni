775ce994e8e7b44ae4b6b90dbb893048c8e183b0
zhenzonglei <zhenzonglei@gmail.com>
2019-01-04 10:00:12 +0800

    merge coords2vol and coords2voi

--- map/map_match_coords2surf.m
+++ map/map_match_coords2surf.m
@@ -1,6 +1,12 @@
-function surf_idx = map_match_coords2surf(coords,surf_file,dist_thr)
-% surf_coords,surf_label is from 
-if nargin < 4, dist_thr = 2; end
+function [coords_vtx,coords_roi,roi_id] = ...
+map_match_coords2surf(coords,surf_file,dist_thr,label_file)
+% [coords_vtx,coords_roi,roi_id] = ...
+% map_match_coords2surf(coords,surf_file,dist_thr,label_file)
+% mapping set of coords to surface vertex and label 
+
+
+if nargin < 4, label_file = []; end
+if nargin < 3, dist_thr = 2; end
 
 % load surf geometry
 surf = gifti(surf_file);
@@ -8,6 +14,31 @@ surf_coords = double(surf.vertices);
 
 % find nearest surf node for each coords
 D = pdist2(coords,surf_coords);
-surf_idx = any(D < dist_thr,2);
+coords_vtx = find(any(D < dist_thr,2));
 
+n_samp = size(coords,1);
+if isempty(label_file)
+    n_roi = 1;
+    coords_roi = true(n_samp,n_roi);
+    roi_id = 1; 
+    
+else
+    % load surf label
+    surf_label = gifti(label_file);
+    surf_label = surf_label.cdata;
+    roi_id = unique(surf_label);
+    roi_id(roi_id==0) = [];
+    n_roi = length(roi_id);
+    
+    coords_roi = false(n_samp,n_roi);
+    for i = 1:n_roi
+        % get coors for a roi
+        roi_coords = surf_coords(surf_label==roi_id(i),:);
+        
+        % samples near the roi Is assigned to the roi
+        D = pdist2(coords,roi_coords);
+        idx = any(D < dist_thr,2);
+        coords_roi(idx,i) = true;
+    end
+end
 
--- map/map_match_coords2vol.m
+++ map/map_match_coords2vol.m
@@ -1,13 +1,33 @@
-function vol_idx = map_match_coords2vol(coords,vol_file,dist_thr)
-% surf_coords,surf_label is from 
-if nargin < 4, dist_thr = 2; end
+function [coords_vox,coords_roi,roi_id] = map_match_coords2vol(coords,roi_file, dist_thr)
+% [coords_vox,coords_roi,roi_id] = map_match_coords2vol(coords,roi_file, dist_thr)
+% map set of coords to voxel and roi in volume space
 
-% % load surf geometry
-% surf = gifti(surf_file);
-% surf_coords = double(surf.vertices);
-% 
-% % find nearest surf node for each coords
-% D = pdist2(coords,surf_coords);
-% surf_idx = any(D < dist_thr,2);
+if nargin < 3, dist_thr = 2; end
+
+% load roi vol
+ni = niftiRead(roi_file);
+
+% match coords to voxel
+[I,J,K] =  ind2sub(size(ni.data),find(ni.data));
+vol_coords = mrAnatXformCoords(ni.qto_xyz, [I,J,K]);
+D = pdist2(coords,vol_coords);
+coords_vox = find(any(D < dist_thr,2));
+
+% mathc coords to roi
+roi_id = unique(ni.data(:));
+roi_id(roi_id==0) = [];
+n_roi = length(roi_id);
+n_samp = size(coords,1);
+coords_roi = false(n_samp,n_roi);
+for i = 1:n_roi
+    % get mni coors for a roi
+    [I,J,K] =  ind2sub(size(ni.data),find(ni.data == roi_id(i)));
+    roi_mni_coords = mrAnatXformCoords(ni.qto_xyz, [I,J,K]);
+    
+    % samples near the roi Is assigned to the roi
+    D = pdist2(coords,roi_mni_coords);
+    I = any(D < dist_thr,2);
+    coords_roi(I,i) = true;
+end
 
 
