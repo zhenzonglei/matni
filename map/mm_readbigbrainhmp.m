04503c38a3d5aa6a13d707c060c3f629be1e7a57
zhenzonglei <zhenzonglei@gmail.com>
2019-07-19 10:04:39 -0400

    NFH: read bigbrain hmp

--- map/mm_readbigbrainhmp.m
+++ map/mm_readbigbrainhmp.m
@@ -1,15 +1,30 @@
-function hmp = mm_readbigbrainhmp()
-% hmp = mm_readbigbrainhmp()
+function hmp = mm_readbigbrainhmp(norm_axis)
+% hmp = mm_readbigbrainhmp(rm_gs)
 % read bigbrain data,fill zeros profile with nan and split two hemisphre
+if nargin < 1, norm_axis = 'vertical'; end
 
+% load bigbrain hmp
 datadir = '/nfs/e5/stanford/ABA/brainmap/bigbrain';
 hmp = load(fullfile(datadir,'BigBrain_20um_163842_OldSurf_RL_Msp.mat'));
 hmp = hmp.MspMatrix;
 
-% remove vertex which has zeros hmp 
-idx = any(~hmp);
-hmp(:,idx) = NaN;
+% replace zeros profile with nan
+idx = all(hmp,1);
+hmp(:,~idx) = NaN;
+
+% normalize hmp
+if strcmp(norm_axis,'vertial')
+    norm_dim = 1;
+elseif strcmp(norm_axis,'tangent')
+    norm_dim = 2;
+else
+    error('Wrong norm axis: should be vertical or tangent');
+end
+hmp(:,idx) = zscore(hmp(:,idx),0,norm_dim);
+
 
 
 % split hmp into two hemisphere
 hmp = reshape(hmp,100,[],2);
+
+
