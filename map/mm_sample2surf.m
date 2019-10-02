ca106ecf9186d8baf9c4c246f3efa1004716cee5
zhenzonglei <zhenzonglei@gmail.com>
2019-10-02 21:57:03 +0800

    NF: weighted mean

--- map/mm_sample2surf.m
+++ map/mm_sample2surf.m
@@ -1,7 +1,7 @@
 function surf_expr = mm_sample2surf(gii_surf,sample_coords, sample_expr,dist_thr,interp)
 % surf_expr = mm_sample2surf(gii_surf,sample_coords, sample_expr,dist_thr,interp)
-% 
-if nargin < 5, interp  = 'nn';end
+% ineterp, nn, mean, linear
+if nargin < 5, interp  = 'nm';end
 if nargin < 4, dist_thr = 2; end
 
 
@@ -12,7 +12,6 @@ surf_coords = double(gii_surf.vertices);
 n_vtx = length(vertex_coords);
 n_gene = size(sample_expr,2);
 
-% n_samp = size(sample_coords,1);
 
 D = pdist2(sample_coords,surf_coords);
 sI = D < dist_thr;
@@ -21,15 +20,23 @@ surf_expr = zeros(n_vtx,n_gene);
 
 switch interp
     case 'nn'
+        [Y,I] = min(D);
+        surf_expr(Y < dist_thr,:) = sample_expr(I(Y < dist_thr),:);
+        
+    case 'nm'
         for v = 1:length(vI)
             expr = sample_expr(sI(:,vI(v)),:);
             surf_expr(vI(v),:) = mean(expr);
         end
+        
     case 'linear'
+        
         for v = 1:length(vI)
             expr = sample_expr(sI(:,vI(v)),:);
-            surf_expr(vI(v),:) = mean(expr);
+            w = D(,);
+            surf_expr(vI(v),:) = mc_wmean(expr,w);
         end
+        
         
     otherwise
         error('Wrong interp method');
