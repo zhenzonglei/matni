f0ff644b7159bfa1acae7ae6b78f06fb7909a6cf
zhenzonglei <zhenzonglei@gmail.com>
2019-10-04 18:54:31 +0800

    RF: coors2 -> coordson

--- map/mm_sample2surfatlas.m
+++ map/mm_sample2surfatlas.m
@@ -1,5 +1,5 @@
-function [sample_roi_idx,roi_id,roi_name] = mm_sample2surfatlas(coords,hemi,dist_thr)
-%[sample_roi_idx,roi_id,roi_name] = mm_sample2surfatlas(coords,hemi,dist_thr)
+function [sample_roi_idx,roi_id,roi_name] = mm_sample2surfatlas(sample_coords,hemi,dist_thr)
+%[sample_roi_idx,roi_id,roi_name] = mm_sample2surfatlas(sample_coords,hemi,dist_thr)
 if nargin < 3, dist_thr = 5; end
 if nargin < 2 , hemi = 'L'; end;
 brainmap_dir = '/nfs/e5/stanford/ABA/brainmap';
@@ -11,11 +11,11 @@ n_roi = length(roi_name);
 
 switch hemi
     case {'L','R'}
-        [sample_roi_idx,roi_id] = sample2Benson(coords,surf_dir, brainmap_dir,hemi,dist_thr);
+        [sample_roi_idx,roi_id] = sample2Benson(sample_coords,surf_dir, brainmap_dir,hemi,dist_thr);
     case 'LR'
         
-        [sample_roi_idx_L,roi_id_L] = sample2Benson(coords,surf_dir, brainmap_dir,'L',dist_thr);
-        [sample_roi_idx_R,roi_id_R] = sample2Benson(coords, surf_dir,brainmap_dir,'R',dist_thr);
+        [sample_roi_idx_L,roi_id_L] = sample2Benson(sample_coords,surf_dir, brainmap_dir,'L',dist_thr);
+        [sample_roi_idx_R,roi_id_R] = sample2Benson(sample_coords, surf_dir,brainmap_dir,'R',dist_thr);
         sample_roi_idx = sample_roi_idx_L | sample_roi_idx_R;
         roi_id = roi_id_L;
     otherwise
@@ -36,7 +36,7 @@ box off
 fprintf('%d samples on surface in total\n',sum(roi_sample_num));
 
 
-function [sample_roi_idx,roi_id] = sample2Benson(coords,surf_dir, brainmap_dir,hemi,dist_thr)
+function [sample_roi_idx,roi_id] = sample2Benson(sample_coords,surf_dir, brainmap_dir,hemi,dist_thr)
 % surf file
 surf_file = fullfile(surf_dir, ...
     sprintf('S1200.%s.midthickness_MSMAll.32k_fs_LR.surf.gii',hemi));
@@ -48,5 +48,5 @@ label_file = fullfile(brainmap_dir,'benson_retinotopy','fs_LR',...
 
 % Map samples to atlas
 surf = gifti(surf_file); label = gifti(label_file);
-[~,coords_roi,roi_id] = mm_coords2surf(coords,surf,dist_thr,label);
+[~,coords_roi,roi_id] = mm_coords2surf(sample_coords,surf,dist_thr,label);
 sample_roi_idx = coords_roi;
